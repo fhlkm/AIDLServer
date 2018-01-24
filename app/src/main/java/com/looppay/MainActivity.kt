8109436e9bc76e7de295b8dda604a0891e9b4672
SISA\hanlu.feng <hanlu.feng@samsungpay.com>
2018-01-24 11:44:02 -0500

    ask permission

--- app/src/main/java/com/looppay/MainActivity.kt
+++ app/src/main/java/com/looppay/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.looppay
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import com.looppay.data.DeviceResult
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     private var TAG = "MainActivity"
@@ -28,6 +33,30 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        ask_permission.setOnClickListener {
+            askTakePciturePermission();
+        }
 
     }
+
+
+    fun askTakePciturePermission(){
+        val permissionCheck = ContextCompat.checkSelfPermission(this@MainActivity,
+                Manifest.permission.CAMERA);
+        if(permissionCheck == PackageManager.PERMISSION_GRANTED){
+           Log.i(TAG,"Take picture right is granted")
+        }else{
+            ActivityCompat.requestPermissions(this,  arrayOf( Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE ), 0)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        if (requestCode == 0) {
+            if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                    && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                Log.i(TAG,"Take picture right is granted")
+            }
+        }
+    }
+
 }
